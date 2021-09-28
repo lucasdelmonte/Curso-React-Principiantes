9021d61b41f374513bb9dd4c30fdb898e4eaa835
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-28 16:06:09 -0300

    Creamos nuestro primer componente.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+// Componente con primer letra en mayusculas (buena practica)
+function Helloworld(){
+  return (
+    <div>Hello World</div> 
+  )
+}
+
 // Permite devolver 'que es' lo que queremos pintar por pantalla
 function App() {
   return (
-    <div>Hello world</div>
+    <div>This is my component: <Helloworld/></div>
   );
 }
 
