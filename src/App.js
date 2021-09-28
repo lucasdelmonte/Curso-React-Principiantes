7a078e91155293bd2daa1adaf82818456af1cb4c
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-28 16:21:31 -0300

    Implementamos props para cambiar el texto de cada componente.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React from 'react';
 import './App.css';
 
 // Componente con primer letra en mayusculas (buena practica)
-function Helloworld(){
+function Helloworld(props){
+  console.log(props);
   return (
-    <div id="hello">Hello World</div> 
+    <div id="hello">{props.mytext}</div> 
   )
 }
 
-// Componente App mediante una arrow function
-// const App = () => <div>This is my component: <Helloworld/> </div>
-
-// Herencia desde React.Component a la clase App
-class App extends React.Component{
-  render(){
-    return <div>This is my component: <Helloworld/> </div>
-  }
-}
 
 // Permite devolver 'que es' lo que queremos pintar por pantalla
-//function App() {
-//  return (
-//    <div>This is my component: <Helloworld/> </div>
-//  );
-//}
+function App() {
+  return (
+    <div>
+      This is my component: 
+      <Helloworld mytext="Hello Fazt" /> 
+      <Helloworld mytext="Hello Luke" /> 
+      <Helloworld mytext="Hello React" /> 
+    </div>
+  );
+}
 
 export default App;
