5fd1ea824add891834124e34cf04376c652f75da
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-29 18:14:13 -0300

    Evento onSubmit y onChange para el formulario.

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
+    
+    state = {
+        title: '',
+        description: ''
+    }
+    
     onSubmit = e => {
-        console.log('Submiting...');
+        console.log(this.state);
         e.preventDefault();
+    }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
     }
 
     render() {
         return (
             <form onSubmit={this.onSubmit}>
-                <input type="text" placeholder="Write a Task" />
+                <input 
+                    type="text"
+                    name="title" 
+                    placeholder="Write a Task" 
+                    onChange={this.onChange} 
+                    value={this.state.title} />
                 <br />
                 <br />
-                <textarea placeholder="Write a Description" />
+                <textarea 
+                    name="description"
+                    placeholder="Write a Description" 
+                    onChange={this.onChange} 
+                    value={this.state.description} />
                 <br />
-                <input type="submit" />
+                <input type="submit"  />
             </form>
         )
     }
