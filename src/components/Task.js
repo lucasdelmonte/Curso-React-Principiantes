4d57bcbc3ab66477f21b02ab90fb27b5a1311e80
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-29 16:31:27 -0300

    Se emprolijo el codigo anteriormente subido.

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 
 class Task extends Component {
   render() {
-     return <div>
-        {this.props.task.title} - {this.props.task.description} - {this.props.task.done} - {this.props.task.id}
+
+    const {task} = this.props;
+
+    return <div>
+        {task.title} - 
+        {task.description} - 
+        {task.done} - 
+        {task.id}
         <input type="checkbox" />
         <button>
           x
