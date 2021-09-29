63f89fec45c596e6d2c5943b969f2b3b53d07bba
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-29 16:43:56 -0300

    Estilo para el parrafo mediante un objeto de javascript.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 
+import './Task.css';
+
+
 class Task extends Component {
   render() {
-
-    const {task} = this.props;
-
-    return <div>
-        {task.title} - 
-        {task.description} - 
-        {task.done} - 
+    const { task } = this.props;
+    const redColor = {background: 'red'};
+    return (
+      <p style={redColor}>
+        {task.title} -
+        {task.description} -
+        {task.done} -
         {task.id}
         <input type="checkbox" />
-        <button>
-          x
-        </button>
-      </div>
-  } 
+        <button>x</button>
+      </p>
+    );
+  }
 }
 
 export default Task;
